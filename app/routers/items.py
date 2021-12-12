3deb41df2cc78f3f1bd440471f606231b879093e
easywaldoji <easywaldoji@gmail.com>
2021-12-13 01:01:02 +0900

    Item Model 에 대한 Mongodb Collection 저장

--- app/routers/items.py
+++ app/routers/items.py
@@ -1,17 +1,44 @@
 from typing import Optional
-from fastapi import FastAPI, BackgroundTasks
-from pydantic import BaseModel
+from fastapi import FastAPI, BackgroundTasks, status
+from pydantic import BaseModel, Field
+from bson import ObjectId
 from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
+
+import os
+
+from starlette.responses import JSONResponse
+import motor.motor_asyncio
 
 app = FastAPI()
 router = APIRouter()
 
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_SAMPLE_URL"])
+db = client.easywaldo
 
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 class Item(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str
     price: float
     isOffer: Optional[bool] = None
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
 
 pre_db = [
    {"name": "waldo", "price": 10000, "isOffer": True},
@@ -25,6 +52,14 @@ def read_root():
 @router.get("/item/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@router.post("/item/create/")
+async def create_item(createItemCommand: Item):
+    item = jsonable_encoder(createItemCommand)
+    newItem = await db["item"].insert_one(item)
+    cretedItem = await db["item"].find_one({"_id": newItem.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=cretedItem)
+
 
 @router.put("/item/{item_id}")
 def update_item(item_id: int, item: Item):
