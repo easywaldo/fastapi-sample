c311d1eb3debb8b2cfd9b09ac96f335b418178a1
easywaldoji <easywaldoji@gmail.com>
2021-12-13 00:00:15 +0900

    mongodb connection

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from fastapi import Depends, FastAPI
 from app.routers import items, users
 
+
+
 app = FastAPI()
 
 app.include_router(users.router, prefix="/users", tags=["users"])
