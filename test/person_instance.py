367110199ace2d5a68277e6cf1c7bc20c0b28464
easywaldo <easywaldoji@gmail.com>
2022-01-16 23:17:15 +0900

    Class instance list sample

--- test/person_instance.py
+++ test/person_instance.py
@@ -4,4 +4,9 @@ person1 = Person("tom",  33, ["python", "java"])
 
 print(person1)
 
-
+personList = []
+personList = [Person("jane", 40, ["c#", "ruby"]), Person("nerma", 40, ["c#", "php"])]
+print(personList[0].name)
+print(personList[1].name)
+personList.append(Person("bob", 40, ["java", "php", "pyton"]))
+print(personList[2].name)
