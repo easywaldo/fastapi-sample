7f017d22b3d4cd0ac7fc014b36545e0964010f2d
jn.lee <jinam@socialbean.co.kr>
2022-01-21 16:38:15 +0900

    db session 에 대한 의존성 주입 추가

--- app/lecture/lectureService.py
+++ app/lecture/lectureService.py
@@ -2,7 +2,6 @@ from sqlalchemy.orm import Session
 
 from app.lecture.lectureEntity import Lecture
 
-
 class LectureService:
   def insert_lecture(self, db: Session, lecture: Lecture):
     db.add(lecture)
--- app/routers/lectures.py
+++ app/routers/lectures.py
@@ -1,19 +1,29 @@
-from fastapi import FastAPI, Body, BackgroundTasks, status, HTTPException
+from fastapi import Depends, FastAPI, Body, BackgroundTasks, status, HTTPException
 from fastapi import Body, APIRouter
 
 import sqlalchemy as db
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (Table, Column, String, Integer, MetaData, select, func)
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 from ..lecture.lectureService import LectureService
 from ..lecture.lectureEntity import Lecture
+
+from ..database import SessionLocal, engine
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 router = APIRouter()
 engine = db.create_engine(SQLALCHEMY_DATABASE_URL)
 Session = sessionmaker(bind = engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @router.post('/lecture/register', tags=['lectures'])
@@ -26,10 +36,10 @@ async def lectureRegister():
 
 
 @router.get('/lecture/{lecture_id}', tags=['lectures'])
-def findLecture(lecture_id: int):
-  session = Session()
+def findLecture(lecture_id: int, db: Session = Depends(get_db)):
+  ##session = Session()
   lectureService = LectureService()
-  return lectureService.find_lecture(session, lecture_id)
+  return lectureService.find_lecture(db, lecture_id)
 
 @router.delete('/lecture/{lectureId}', tags=['lectures'])
 def deleteLecture(lectureId: int):
@@ -37,3 +47,10 @@ def deleteLecture(lectureId: int):
   lectureService = LectureService()
   lectureService.delete_lecture(session, lectureId)
   return "success"
+
+@router.get('/lecture/list', tags=['lectures'])
+async def lectureList():
+  session = Session()
+  lectureService = LectureService()
+  result = lectureService.list_lecture(session)
+  return "test"
